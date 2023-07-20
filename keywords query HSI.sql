bf38f7f89c6e52cba62df864b3cddd7b0073bb4a
Douglas Wang <132033521+DaotanWang@users.noreply.github.com>
2023-07-20 11:14:26 +0800

    Update keywords query HSI.sql

--- keywords query HSI.sql
+++ keywords query HSI.sql
@@ -1,13 +1,15 @@
+SET @site = 'ph';
+SET @product_model = 'HUAWEI Mate 50'; 
 SELECT Site, product_model, Date, SUM(hsi) as hsi
 FROM 
 (
-  SELECT 'ph' as Site,'HUAWEI Mate 50' as product_model, Date, hsi
+  SELECT @site as Site, @product_model as product_model, Date, hsi
   FROM hsi_history.ph_hsi 
   WHERE query IN 
   (
     SELECT keywords 
     FROM hsi_history.keywords
-    WHERE product_model = 'HUAWEI Mate 50' and site_code = 'ph'
+    WHERE product_model = @product_model and site_code = @site
   )
 ) tmp 
 GROUP BY Date
